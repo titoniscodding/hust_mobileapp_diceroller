e72c79f6dfd228d628a62cfd9422f8d6184f3105
titoniscodding <nammun1712@gmail.com>
2023-10-09 05:55:11 +0700

    roll

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(/* layoutResID = */ R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button3)
+        val rollButton2: Button = findViewById(R.id.button6)
+        rollButton.setOnClickListener { rollDice() }
+        rollButton2.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        // create 2 new dice object and roll
+        val dice1 = Dice(6)
+        val dice2 = Dice(6)
+        val dice1Roll = dice1.roll()
+        val dice2Roll = dice2.roll()
+
+        // update screen with result
+        val resultTextView1: TextView = findViewById(R.id.textView)
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        resultTextView1.text = dice1Roll.toString()
+        resultTextView2.text = dice2Roll.toString()
+    }
+
+    private class Dice(val numSides: Int) {
+
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+}
+
